ef0ae285553025ac8a460a6ad01a348d37a6f586
krishshah99615 <krishshah99615@gmail.com>
2020-03-24 17:14:09 +0530

    OPtimized the code

--- a.py
+++ a.py
@@ -2,8 +2,8 @@ import pandas as pd
 from pandas_datareader import data 
 import matplotlib.pyplot as plt 
 
-
-def get_stock_data(start,end,loc): 
+#Download all the stock data
+def stock_data(start,end,loc): 
 
 	date_index = pd.date_range(start,end)
 	df1=pd.DataFrame(index=date_index)
@@ -13,117 +13,104 @@ def get_stock_data(start,end,loc):
 		df1.rename(columns={'Close':f'{sym}'},inplace=True)
 	return df1.dropna()
 
-
+#Calculate Rolling Mean
+def rollingMean(df):
     
-def rolling_mean(df):
+    return df.rolling(20).mean() 
+#Calculate Rolling Standard deviation
+def rollingStd(df):
     
-    com_rm=df.rolling(20).mean()
-    return com_rm  
-def rolling_std(df):
+    return df.rolling(20).std()
+      
+#Calculate Daily return
     
-    com_rstd=df.rolling(20).std()
-    return com_rstd 
-
-
-
-def daily_return(df):
-    df=((df.shift(1)/df)-1)*100
+def dailyReturn(df):
+    df=((df/df.shift(1))-1)*100
    
     return df
-def cumulative_return(df):
+
+#Calculate Cumulative Return
+
+def cumulativeReturn(df):
     df=((df/df.iloc[0])-1)*100
     
     return df
 
+#Calculating bollinger bands
+def bolingerBand(df):
     
-def global_stats(df):
-    mean = df.mean()
-    median = df.median()
-    standard_d= df.std()
+    x = df
+   #Calculate upper band
+    upper_rolling=rollingMean(x)+(rollingStd(x)*2)
     
-    print("Mean of the stocks \n"+ str(mean))
+    #Calculate lower band
+    lower_rolling=rollingMean(x)-(rollingStd(x)*2)
     
-    print("Median of the stocks \n"+ str(median))
     
-    print("Standard dev of the stocks \n"+ str(standard_d))
-   
+    return upper_rolling,lower_rolling
 
-
-
-        
 def plotting_whole(df,l):
     for com in l:
         x= df[com]
+        u,l=bolingerBand(x)
+        dr = dailyReturn(x)
         
-        fig,ax = plt.subplots(2,2)
+        fig,ax = plt.subplots(6,figsize=(20,20))
+    
         
         #Plot stock close price
-        ax[0,0].plot(x.index,x.values)
-        ax[0,0].set_xlabel("Date")
-        ax[0,0].set_ylabel("Price")
-        ax[0,0].set_title(f"Stock Price {com}")
+        ax[0].plot(x.index,x.values)
+        ax[0].set_xlabel("Date")
+        ax[0].set_ylabel("Price")
+        ax[0].set_title(f"Stock Price {com}")
         
         
         
         #Plot rolling mean
-        ax[0,1].plot(rolling_mean(x).index,rolling_mean(x).values)
-        ax[0,1].set_xlabel("Date")
-        ax[0,1].set_ylabel("Price")
-        ax[0,1].set_title(f"Rolling Mean {com}")
+        ax[1].plot(rollingMean(x).index,rollingMean(x).values)
+        ax[1].set_xlabel("Date")
+        ax[1].set_ylabel("Price")
+        ax[1].set_title(f"Rolling Mean {com}")
         
         
         #Plot daily return
-        ax[1,0].plot(daily_return(x).index,daily_return(x).values)
-        ax[1,0].set_xlabel("Date")
-        ax[1,0].set_ylabel("Percentage")
-        ax[1,0].set_title(f"Daily Return {com}")
+        ax[2].plot(dailyReturn(x).index,dailyReturn(x).values)
+        ax[2].set_xlabel("Date")
+        ax[2].set_ylabel("Percentage")
+        ax[2].set_title(f"Daily Return {com}")
         
         #Plot cumulative return
-        ax[1,1].plot(cumulative_return(x).index,cumulative_return(x).values)
-        ax[1,1].set_xlabel("Date")
-        ax[1,1].set_ylabel("Percentage")
-        ax[1,1].set_title(f"Cumulative Return {com}")
+        ax[3].plot(cumulativeReturn(x).index,cumulativeReturn(x).values)
+        ax[3].set_xlabel("Date")
+        ax[3].set_ylabel("Percentage")
+        ax[3].set_title(f"Cumulative Return {com}")
+        
+        #Plot Bollinger band
+        ax[4].plot(rollingMean(x).index,rollingMean(x).values)
+        ax[4].plot(u.index,u.values)
+        ax[4].plot(l.index,l.values)
+        ax[4].set_xlabel("Date")
+        ax[4].set_ylabel("Price")
+        ax[4].set_title(f"Bollinger Bands  {com}")
+        
+        #Plot histogram of daily return
+        ax[5] = dr.hist(bins=20)
+        ax[5].axvline(dr.mean(),color='w',linestyle='dashed',linewidth=2)
+        ax[5].axvline(dr.std(),color='r',linestyle='dashed',linewidth=2)
+        ax[5].axvline(-dr.std(),color='r',linestyle='dashed',linewidth=2)
+        
         plt.show()
         
         plt.savefig(f'{com}.png')
     
-        #Plot Bolinger Bands
-        bolinger_band(x)
-        
-        #PLot daily return histogram
-        p = daily_return(x)
-        h=p.hist(bins=20)
-        h.axvline(p.mean(),color='w',linestyle='dashed',linewidth=2)
-        h.axvline(p.std(),color='r',linestyle='dashed',linewidth=2)
-        h.axvline(-p.std(),color='r',linestyle='dashed',linewidth=2)
-        plt.show()
-        
-
-        
-def bolinger_band(df):
-    
-    x = df
-   
-    upper_rolling=rolling_mean(x)+(rolling_std(x)*2)
-    lower_rolling=rolling_mean(x)-(rolling_std(x)*2)
-    
-    x.plot()
-    rolling_mean(x).plot()
-    upper_rolling.plot()
-    lower_rolling.plot()
-    plt.show()
-
-
 ticker = ['CSCO','GOOGL']
 
 start='2010-01-25'
 end='2010-12-25'  
     
-a = get_stock_data(start,end,ticker)
+a = stock_data(start,end,ticker)
 
 plotting_whole(a,ticker)
-
-plt.scatter(daily_return(a['GOOGL']),daily_return(a['CSCO']))
-plt.show()
-
-
+        
+       
+        
